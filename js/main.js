cb9f316ba0fe03c25f490361a3f3937f3069fc3f
Анна Абакумова <pin0k@yandex.ru>
2022-09-16 09:43:37 +0300

    Добавление функции, создающей массив из случайных чисел

--- js/main.js
+++ js/main.js
@@ -10,10 +10,55 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-alert(getRandomInt(2, 0));
-
 const getStringCount = (text, maxLength) => {
   return (text.trim().length <= maxLength);
 };
 
 alert(getStringCount('проверочное сообщение', 5));
+
+// Начало третьей домашки
+const NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+const SURNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг',
+];
+
+const TEXT_MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+// Функция для создания массива случайных чисел
+
+const SIMILAR_OBJECT_COUNT = 25;
+
+const UNIQUE_INTEGER = [];
+
+const getRandomUniqueInteger = (count) => {
+  for (let i = 0; i < count; i++) {
+    UNIQUE_INTEGER.push(getRandomInt(1, 1000));
+  };
+}
+
+getRandomUniqueInteger(SIMILAR_OBJECT_COUNT);
+console.log(UNIQUE_INTEGER);
